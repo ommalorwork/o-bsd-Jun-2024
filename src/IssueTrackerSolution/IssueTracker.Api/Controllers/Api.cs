2c3d434b404de6517ce1566b75662dca37c4d81d
ommalorwork <ommalorwork@gmail.com>
2024-06-10 16:49:32 -0400

    Second change

---
+++ src/IssueTrackerSolution/IssueTracker.Api/Controllers/Api.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTracker.Api.Status;
+
+public class Api : ControllerBase
+{
+    // GET /status
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetTheStatusAsync()
+    {
+        var response = new GetStatusResponse("Looks Good Here!", DateTimeOffset.UtcNow);
+        return Ok(response); // 
+    }
+}
+
+public record GetStatusResponse(string Message, DateTimeOffset LastChecked);
